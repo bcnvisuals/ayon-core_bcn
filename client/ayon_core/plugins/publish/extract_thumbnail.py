1b4ca94a9c4d5f4f66c87a5c80fc169d92e40a88
George Antonopoulos <george.antonopoulos@bcnvisuals.com>
2024-06-25 12:07:34 +0000

    include houdini for thumbnails

--- client/ayon_core/plugins/publish/extract_thumbnail.py
+++ client/ayon_core/plugins/publish/extract_thumbnail.py
@@ -36,7 +36,8 @@ class ExtractThumbnail(pyblish.api.InstancePlugin):
         "traypublisher",
         "substancepainter",
         "nuke",
-        "aftereffects"
+        "aftereffects",
+        "houdini"
     ]
     enabled = False
 
